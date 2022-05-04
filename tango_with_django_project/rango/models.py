3917ea8d1021477294469389b8c57ae3307f7782
Daniella <daniella.vasquez@utp.ac.pa>
2022-05-04 10:48:49 -0500

    implementación de la base de datos

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,21 @@
+from unicodedata import category
+from unittest.util import _MAX_LENGTH
 from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+
+    def _str_(self):
+        return self.title
+
 
 # Create your models here.
